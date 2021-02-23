fd08d5673a0e219cff145da888be2ea34bde9da9
Onegin Aleksandr <perm.oas@gmail.com>
2021-02-23 12:10:09 +0500

    с нормировков

--- Project3.2/Project3.2/Source.cpp
+++ Project3.2/Project3.2/Source.cpp
@@ -1,47 +1,52 @@
 #include <iostream>
 const int precision = 5;
 const int slots = 128;
-int hash(double d)
+
+
+//int hash(double d)
+//{
+//	double mant = abs(d);
+//	int exp = 0;
+//	int h = (d>0.0)?1:0;
+//	if (mant > 1.0)
+//	{
+//		for (exp; mant > 1.0; exp++)
+//		{
+//			mant /= 10;
+//		}
+//	}
+//	else
+//	{
+//		for (exp; mant < 1.0; exp--)
+//		{
+//			mant *= 10;
+//		}
+//	}
+//	mant *= 10;
+//	d--;
+//	for (int i = 0; i < precision; i++)
+//	{
+//		h = h * 51 + floor(mant);
+//		mant = (mant - floor(mant)) * 10;
+//	}
+//	return (h * 51 + exp)%slots;
+//		
+//}
+
+int hash(double a, double b, int H, double d)
 {
-	double mant = abs(d);
-	int exp = 0;
-	int h = (d>0.0)?1:0;
-	if (mant > 1.0)
-	{
-		for (exp; mant > 1.0; exp++)
-		{
-			mant /= 10;
-		}
-	}
-	else
-	{
-		for (exp; mant < 1.0; exp--)
-		{
-			mant *= 10;
-		}
-	}
-	mant *= 10;
-	d--;
-	for (int i = 0; i < precision; i++)
-	{
-		h = h * 51 + floor(mant);
-		mant = (mant - floor(mant)) * 10;
-	}
-	return (h * 51 + exp)%slots;
+	return floor(((d - a) / (b - a)) * H);
 
-
-	
 }
-
 
 
 int main()
 {
-	std::cout << "hash( " << 33.14 << " ) = " << hash(33.14) << std::endl;
-	std::cout << "hash( " << -33.14 << " ) = " << hash(-33.14) << std::endl;
-	std::cout << "hash( " << 3.14 << " ) = " << hash(3.14) << std::endl;
-	std::cout << "hash( " << 3.139 << " ) = " << hash(3.139) << std::endl;
-	std::cout << "hash( " << 3.1411 << " ) = " << hash(3.1411) << std::endl;
-	std::cout << "hash( " << 3.14112 << " ) = " << hash(3.14112) << std::endl;
+	std::cout << "hash( " << 33.14 << " ) = " << hash(-50, 50, 97, 33.14) << std::endl;
+	std::cout << "hash( " << -33.14 << " ) = " << hash(-50, 50, 97 ,-33.14) << std::endl;
+	std::cout << "hash( " << 3.14 << " ) = " << hash(3.13, 3.15, 97, 3.14) << std::endl;
+	std::cout << "hash( " << 3.139 << " ) = " << hash(3.13, 3.15, 97, 3.139) << std::endl;
+	std::cout << "hash( " << 3.1411 << " ) = " << hash(3.13, 3.15, 97,3.1411) << std::endl;
+	std::cout << "hash( " << 3.14112 << " ) = " << hash(3.14, 3.142, 97,3.14112) << std::endl;
 
 }
