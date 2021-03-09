b77676c841f791ec8de22d47bbeac88c44b9e7e1
Onegin Aleksandr <perm.oas@gmail.com>
2021-03-09 14:13:09 +0300

    currency

---
+++ Project5.1/Project5.1/Source.cpp
@@ -0,0 +1,43 @@
+﻿#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <locale>
+#include <sstream>
+
+#include <Windows.h> 
+
+int main(int argc, char** argv)
+{
+
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+
+
+	std::istringstream in("123456 €");
+	std::string v3;
+	in.imbue(std::locale("deu_deu.UTF-8"));
+	in >> std::get_money(v3, false);
+	if (in)
+	{
+		std::ostringstream sout;
+		sout.imbue(std::locale("ru_Ru.utf8"));
+		sout << std::showbase << std::put_money(v3, false) << std::endl;
+
+		sout.imbue(std::locale("sv_SE.utf8"));
+		sout << std::showbase << std::put_money(v3, false) << std::endl;
+
+		sout.imbue(std::locale("ja_JP.utf8"));
+		sout << std::showbase << std::put_money(v3, false) << std::endl;
+
+		sout.imbue(std::locale("en_US.utf8"));
+		sout << std::showbase << std::put_money(v3, false) << std::endl;
+		std::cout << sout.str() << std::endl;
+	}
+	else
+	{
+		std::cout << "Parse failed\n";
+	}
+
+
+}
