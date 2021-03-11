48bdd87aca572c83e273e0ac67768cabee55f93c
Onegin Aleksandr <perm.oas@gmail.com>
2021-03-11 16:47:47 +0300

    done

--- Project5.3/Project5.3/Source.cpp
+++ Project5.3/Project5.3/Source.cpp
@@ -9,7 +9,7 @@ int main(int argc, char** argv)
 	//check domains: alnums or . or-, check dots and -
 	std::string data = "check usernames 1.1.1.1@1.1 n@1 ._1.1.1@1.12 1.1..1.@1 1.1.1@11.13.23.123456; check domains 1@1.1 1@.1.1.1234567  1@1..1 1@1.1. PP 1@1-1 1@-1.1  1@1--1 1@-1.1.-";
 
-	std::regex pattern(R"(((\w+[.])*(\w+)@(\w+[.])*(\w{2,6})))");
+	std::regex pattern(R"(((\w+[.])*(\w+)@(\w+[.])+(\w{2,6})))");
 
 	std::sregex_iterator begin(data.cbegin(), data.cend(), pattern);
 	std::sregex_iterator end;
