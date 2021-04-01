5f0f3dcf32d4906e0616e742cb0d33ab94b7821a
Onegin Aleksandr <perm.oas@gmail.com>
2021-04-01 16:59:02 +0300

    modified

--- Project8.4/Project8.4/Source.cpp
+++ Project8.4/Project8.4/Source.cpp
@@ -11,6 +11,9 @@
 #include <boost/lockfree/stack.hpp>
 
 //=======================================================================
+
+std::atomic < bool > flag = false;
+
 template <typename T >
 class Timer
 {
@@ -36,7 +39,9 @@ public:
 
 
 	Timer(std::string Name) : m_begin(clock_t::now()), m_duration(0), is_stopped(false), name(Name)
-	{}
+	{
+		std::cout << name << " has started\n";
+	}
 
 	~Timer() noexcept
 	{
@@ -46,7 +51,7 @@ public:
 			{
 				auto end = clock_t::now();
 				m_duration += std::chrono::duration_cast<duration_t>(end - m_begin);
-				std::cout <<  m_duration.count() << ',';
+				std::cout <<name << " is stopped: " << m_duration.count() << std::endl;;
 			}
 
 		}
@@ -136,122 +141,121 @@ private:
 };
 //========================================================================
 
-std::condition_variable cv;
-std::mutex cv_m;
-
-template < typename T >
-void produce(unsigned int M, T &stack)
+void produce(unsigned int M, Threadsafe_Stack<int> &stack)
 {
-	std::unique_lock<std::mutex> lk(cv_m);
-	cv.wait(lk);
+	while (!flag)
+		std::this_thread::yield();
+
 	for (auto i = 0u; i < M; i++)
+	{
 		stack.push(i);
+
+		std::this_thread::yield();
+	}
 }
 
-template < typename T >
-void consume(unsigned int M, T &stack)
+void consume(unsigned int M, Threadsafe_Stack<int>& stack)
 {
-	std::unique_lock<std::mutex> lk(cv_m);
-	cv.wait(lk);
-	for (auto i = 0u; i < M; i++)
+	while (!flag)
+		std::this_thread::yield();
+
+
+	for (unsigned i = 0; i < M; ++i)
+	{
 		stack.pop();
+		std::this_thread::yield();
+
+	}
+
 }
-
-void signals()
+//=========================================================
+void boostproduce(unsigned int M, boost::lockfree::stack<int>& stack)
 {
-	cv.notify_all();
-}
+	while (!flag)
+		std::this_thread::yield();
 
-
-
-template < typename T >
-void consume1(unsigned int M, T& stack)
-{
-	std::unique_lock<std::mutex> lk(cv_m);
-	cv.wait(lk);
-	int value;
 	for (auto i = 0u; i < M; i++)
-		stack.pop(value);
+	{
+		stack.push(i);
+
+		std::this_thread::yield();
+	}
 }
 
+void boostconsume(unsigned int M, boost::lockfree::stack<int>& stack)
+{
+	while (!flag)
+		std::this_thread::yield();
 
+	int value;
+	for (unsigned i = 0; i < M; ++i)
+	{
+		stack.pop(value);
+		std::this_thread::yield();
+
+	}
+
+}
 int main(int argc, char** argv)
 {
-	auto N = 10u;
-	auto M = 25u;
-	
-	/*for (M = 1; M < 1000; M += 25)
+	auto N = 150u;
+	auto M = 1000u;
 	{
+		Threadsafe_Stack < int > stack;
+
+		for (auto i = 0u; i < M * 10; i++)
 		{
-			Timer<std::chrono::microseconds> t1("Threadsafe_stack");
-			Threadsafe_Stack < int > stack;
-
-			for (auto i = 0u; i < 30; i++)
-			{
-				stack.push(i);
-			}
-
-			std::vector<std::thread> consumers(N);
-			for (auto i = 0u; i < consumers.size(); i++)
-			{
-				consumers[i] = std::thread(consume<Threadsafe_Stack<int>>, M, std::ref(stack));
-			}
-
-			std::vector<std::thread> producers(N);
-			for (auto i = 0u; i < producers.size(); i++)
-			{
-				producers[i] = std::thread(produce<Threadsafe_Stack<int>>, M, std::ref(stack));
-			}
-
-
-			std::thread signal(signals);
-			signal.join();
-
-
-			std::for_each(producers.begin(), producers.end(), [](auto& thread) {thread.join(); });
-			std::for_each(consumers.begin(), consumers.end(), [](auto& thread) {thread.join(); });
-
+			stack.push(i);
 		}
 
-	}*/
-	for (M = 1; M < 1000; M += 25) 
+		std::vector<std::thread> consumers(N);
+		for (auto i = 0u; i < consumers.size(); i++)
+		{
+			consumers[i] = std::thread(consume, M, std::ref(stack));
+		}
+
+		std::vector<std::thread> producers(N);
+		for (auto i = 0u; i < producers.size(); i++)
+		{
+			producers[i] = std::thread(produce, M, std::ref(stack));
+		}
+
+		Timer<std::chrono::microseconds> t1("Threadsafe_stack");
+
+		flag = true;
+
+		std::for_each(producers.begin(), producers.end(), [](auto& thread) {thread.join(); });
+		std::for_each(consumers.begin(), consumers.end(), [](auto& thread) {thread.join(); });
+	}
+
 	{
-		try
-			{
-				Timer<std::chrono::microseconds> t1("Lockfree_stack");
-				boost::lockfree::stack < int> stack(128);
+		boost::lockfree::stack < int > stack(128);
 
-				for (auto i = 0u; i < 30; i++)
-				{
-					stack.push(i);
-				}
-
-				std::vector<std::thread> consumers(N);
-				for (auto i = 0u; i < consumers.size(); i++)
-				{
-					consumers[i] = std::thread(consume1 <boost::lockfree::stack < int>>,
-						M, std::ref(stack));
-				}
-
-				std::vector<std::thread> producers(N);
-				for (auto i = 0u; i < producers.size(); i++)
-				{
-					producers[i] = std::thread(produce<boost::lockfree::stack <int>>,
-						M, std::ref(stack));
-				}
-
-
-				std::thread signal(signals);
-				signal.join();
-
-
-				std::for_each(producers.begin(), producers.end(), [](auto& thread) {thread.join(); });
-				std::for_each(consumers.begin(), consumers.end(), [](auto& thread) {thread.join(); });
-			}
-			catch (std::exception& ex)
-			{
-				std::cout << ex.what() << std::endl;
-			}
+		for (auto i = 0u; i < M * 10; i++)
+		{
+			stack.push(i);
 		}
+
+		std::vector<std::thread> consumers(N);
+		for (auto i = 0u; i < consumers.size(); i++)
+		{
+			consumers[i] = std::thread(boostconsume, M, std::ref(stack));
+		}
+
+		std::vector<std::thread> producers(N);
+		for (auto i = 0u; i < producers.size(); i++)
+		{
+			producers[i] = std::thread(boostproduce, M, std::ref(stack));
+		}
+
+		Timer<std::chrono::microseconds> t1("Lockfree_Stack");
+
+		flag = true;
+
+		std::for_each(producers.begin(), producers.end(), [](auto& thread) {thread.join(); });
+		std::for_each(consumers.begin(), consumers.end(), [](auto& thread) {thread.join(); });
+
+	}
+
 
 }
