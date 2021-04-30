cd15f356efba0fffa96fd6c48e3ff5763a4a851a
Onegin Aleksandr <perm.oas@gmail.com>
2021-04-30 21:03:19 +0300

    modified

--- 11. BrownPart/ProjectBrownPart/Source.cpp
+++ 11. BrownPart/ProjectBrownPart/Source.cpp
@@ -50,8 +50,8 @@ public:
 
 int main()
 {
-	const int N = 31;
-	const int f = 4; // field f*f
+	const int N = 50;
+	const int f = 6; // field f*f
 
 	const auto width = VideoMode::getDesktopMode().width;
 	const auto height = VideoMode::getDesktopMode().height;
@@ -61,32 +61,41 @@ int main()
 
 	std::vector <std::shared_ptr<Particle>> particle;
 	std::vector <std::shared_ptr<CircleShape>> circle;
-
-	std::vector <std::vector<int>> field(f, std::vector<int>(f,0));
-
-
-
 	particle.reserve(N);
 	circle.reserve(N);
+
+	std::vector <std::vector<int>> field(f, std::vector<int>(f,0));
 
 	std::random_device rd;
 	std::mt19937 mersenne(rd());
 	std::uniform_real_distribution<float> uidx(0,width);
 	std::uniform_real_distribution<float> uidy(0, height);
 
-	std::uniform_real_distribution<float> uidv(-4.0f/10,4.0f/10);
+	std::uniform_real_distribution<float> uidv(-4.0f/5 ,4.0f/5);
 
 	particle.push_back(std::make_shared<Particle>(width / 2, height / 2, 0.1f, 0.1f, 6.0f, 36.0f));
 	circle.push_back(std::make_shared<CircleShape>(particle[0]->m_radius));
 	circle[0]->setFillColor(Color::Magenta);
 
+	double x, y;
 
 	for (auto i = 1U; i < N; i++)
 	{
+		x = uidx(mersenne);
+		y = uidy(mersenne);
+
+		while ((x + width / 2 + particle[0]->m_radius) * (x + width / 2 + particle[0]->m_radius) +
+			(y + height / 2 + particle[0]->m_radius) * (y + height / 2 + particle[0]->m_radius)
+			<= (particle[0]->m_radius + 10.0f) * (particle[0]->m_radius + 10.0f))
+		{
+			x = uidx(mersenne);
+			y = uidy(mersenne);
+		}
 		particle.push_back(std::make_shared<Particle>(uidx(mersenne), uidy(mersenne), uidv(mersenne), uidv(mersenne), 1.0f, 6.0f));
 		circle.push_back(std::make_shared<CircleShape>(particle[i]->m_radius));
 		circle[i]->setFillColor(Color::Blue);
 	}
+
 
 	std::chrono::steady_clock::time_point time_point = std::chrono::steady_clock::now();
 
@@ -94,17 +103,8 @@ int main()
 
 	std::chrono::microseconds timer(0);
 
-	double dx = 0.0005;
 	while (application.isOpen())
-	{
-		for (auto i = 0u; i < f; i++)
-		{
-			for (auto j = 0u; j < f; j++)
-			{
-				field[i][j] = 0;
-			}
-		}
-
+	{	
 		timer += std::chrono::duration_cast <std::chrono::microseconds> (std::chrono::steady_clock::now() - time_point);
 
 		time_point = std::chrono::steady_clock::now();
@@ -119,20 +119,29 @@ int main()
 			}
 		}
 
-		;
+		for (auto i = 0u; i < f; i++)
+		{
+			for (auto j = 0u; j < f; j++)
+			{
+				RectangleShape rectangle(Vector2f(width / f, height / f));
+				rectangle.setPosition(i * width / f, j * height / f);
+				rectangle.setFillColor(Color(std::abs(255 - field[i][j] * 20), 255, 255));
+				application.draw(rectangle);
 
+				field[i][j] = 0;
+			}
+		}
 
-		application.clear(Color::Black);
-
+		
 		for (auto i = 0U; i< particle.size(); i++)
 		{
 			for (auto j = i; j < particle.size(); j++)
 			{
 				if (i!=j)
 					isCollide(particle[i], particle[j]);
-
 			}
 		}
+
 		for (auto i = 0U; i<particle.size(); i++)
 		{			
 			circle[i]->setPosition(particle[i]->m_x - particle[i]->m_radius, particle[i]->m_y - particle[i]->m_radius);
@@ -147,14 +156,11 @@ int main()
 			if (particle[i]->m_y + particle[i]->m_radius > height || particle[i]->m_y - particle[i]->m_radius < 0)
 				particle[i]->m_dy = -particle[i]->m_dy;
 
-			field[std::floor(particle[i]->m_x)/(width/f) ][std::floor(particle[i]->m_y) / (width / f)]++;
+			field[static_cast<int>(particle[i]->m_x / (width / f))][static_cast<int>(particle[i]->m_y / (height / f))]++;
 		}
 
-
-
-		
-
 		application.display();
+				
 	}
 
 	system("pause");
